04277d97eb2219df3f12a73c5ec5fdb08a65a1fd
Mikolaj Dobrucki <mikolaj.dobrucki@gmail.com>
2017-02-19 13:21:08 +0100

    new gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,27 +1,44 @@
 'use strict';
-
 var gulp = require('gulp');
+
+
+
+//// CSS ////
 var sass = require('gulp-sass');
 var sourcemaps = require('gulp-sourcemaps');
 var autoprefixer = require('gulp-autoprefixer');
-var fileinclude = require('gulp-file-include');
-var uglify = require('gulp-uglify');
-var rename = require('gulp-rename');
-var concat = require('gulp-concat');
 
 gulp.task('sass', function () {
   return gulp.src('./scss/**/*.scss')
+    .pipe(sourcemaps.init())
     .pipe(sass({
       outputStyle: 'compressed'
     })
       .on('error', sass.logError))
-    .pipe(sourcemaps.init())
     .pipe(autoprefixer({
       browsers: ['last 2 versions', 'ie >= 10']
     }))
     .pipe(sourcemaps.write('.'))
     .pipe(gulp.dest('./build/css'));
 });
+
+//// JAVASCRIPT ////
+var uglify = require('gulp-uglify');
+var concat = require('gulp-concat');
+
+gulp.task('js', function() {
+  return gulp.src([
+    './js/*.js'
+  ])
+    .pipe(concat('scripts.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('./build/js/'));
+});
+
+
+
+//// HTML ////
+var fileinclude = require('gulp-file-include');
 
 gulp.task('html', function() {
   gulp.src(['./html/*.html'])
@@ -33,19 +50,21 @@ gulp.task('html', function() {
     .pipe(gulp.dest('./build'));
 });
 
-gulp.task('scripts', function() {
-  return gulp.src('./js/*.js')
-    .pipe(concat('scripts.js'))
-    .pipe(gulp.dest('./build/js/'))
-    .pipe(rename('scripts.min.js'))
-    .pipe(uglify())
-    .pipe(gulp.dest('./build/js/'));
-});
 
+
+//// WATCH ALL ////
 gulp.task('watch', function () {
   gulp.watch('./scss/**/*.scss', ['sass']);
   gulp.watch('./html/**/*.html', ['html']);
-  gulp.watch('./js/**/*.js', ['scripts']);
+  gulp.watch('./js/**/*.js', ['js']);
 });
 
-gulp.task('default', ['sass','html','scripts','watch']);
+
+
+//// BUILD ////
+gulp.task('build', ['sass','html','js']);
+
+
+
+//// DEFAULT – COMPILE ALL AND BUILD ////
+gulp.task('default', ['sass','html','js','watch']);
